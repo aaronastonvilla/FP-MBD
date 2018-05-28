91ec59a2bb611b977df5815112bf08509d0e8f3a
Aufa Wibowo <muh.aufa@gmail.com>
2018-05-28 13:49:20 +0700

    1
    1

---
+++ query.sql
@@ -0,0 +1,15 @@
+CREATE TABLE event_login(
+el_id INT,
+el_user VARCHAR(30),
+el_pwd VARCHAR(30),
+PRIMARY KEY (el_id)
+);
+
+CREATE TABLE tenant_login(
+tl_id INT,
+tl_user VARCHAR(30),
+tl_pwd VARCHAR(30),
+PRIMARY KEY (tl_id)
+);
+
+CREATE TABLE 
